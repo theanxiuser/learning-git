dea4d2e1ce461193bb6c67f82fa55e01637bbe46
theanxiuser <theanxiuser@gmail.com>
2025-12-01 10:59:31 +0545

    reafactor: new line added in hello.py

--- hello.py
+++ hello.py
@@ -7,3 +7,5 @@ print("this the code for the updating and requesting the pull request in git hub
 print(" hello samita, You are doing well good job padhnu parxa")
 
 print("this is me kishori gurung ")
+
+print("Hi there")
